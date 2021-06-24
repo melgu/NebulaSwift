272c01f7b8328ab713d872814ee257706d7a3c71
Melvin Gundlach <mail@melvin-gundlach.de>
2021-06-25 00:10:33 +0200

    Initial Commit

---
+++ Shared/NebulaSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  NebulaSwiftApp.swift
+//  Shared
+//
+//  Created by Melvin Gundlach on 25.06.21.
+//
+
+import SwiftUI
+
+@main
+struct NebulaSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
