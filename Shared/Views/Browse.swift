4e7ed5eb8edeed1eed99dda799cf7eda8330187c
Melvin Gundlach <mail@melvin-gundlach.de>
2022-04-29 19:27:40 +0200

    Add switch between videos and channels in Browse

--- Shared/Views/Browse.swift
+++ Shared/Views/Browse.swift
@@ -11,15 +11,43 @@ struct Browse: View {
 	@EnvironmentObject private var api: API
 	@EnvironmentObject private var player: Player
 	
+	@State private var viewType: ViewType = .videos
+	
     var body: some View {
-		AutoVideoGrid(fetch: { page in
-			try await api.allVideos(page: page)
-		})
+		Group {
+			switch viewType {
+			case .videos:
+				AutoVideoGrid(fetch: { page in
+					try await api.allVideos(page: page)
+				})
+			case .channels:
+				AutoChannelGrid(fetch: { page in
+					try await api.allChannels(page: page)
+				})
+			}
+		}
 		.navigationTitle("Browse")
+		.toolbar {
+			switch viewType {
+			case .videos:
+				Button("Switch to Channels") {
+					viewType = .channels
+				}
+			case .channels:
+				Button("Switch to Videos") {
+					viewType = .videos
+				}
+			}
+		}
 		.onAppear {
 			player.reset()
 		}
     }
+}
+
+fileprivate enum ViewType {
+	case videos
+	case channels
 }
 
 struct Browse_Previews: PreviewProvider {
