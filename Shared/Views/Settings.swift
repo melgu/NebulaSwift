52e11997b778a0807d1e3700a39f879345311a9d
Melvin Gundlach <mail@melvin-gundlach.de>
2022-06-01 23:16:41 +0200

    Add SwiftUIPullToRefresh license to Settings

--- Shared/Views/Settings.swift
+++ Shared/Views/Settings.swift
@@ -35,14 +35,51 @@ struct SettingsView: View {
 	
 	var content: some View {
 		List {
-			Button {
-				dismiss()
-				player.reset()
-				api.logout()
-			} label: {
-				Text("Logout")
+			Section("User") {
+				Button {
+					dismiss()
+					player.reset()
+					api.logout()
+				} label: {
+					Text("Logout")
+				}
+				.disabled(!api.isLoggedIn)
 			}
-			.disabled(!api.isLoggedIn)
+			
+			Section("Third-Party-Licenses") {
+				NavigationLink("SwiftUIPullToRefresh") {
+					ScrollView {
+						Text(
+							"""
+							MIT License
+
+							Copyright (c) 2021 Gordan Glavaš
+
+							Permission is hereby granted, free of charge, to any person obtaining a copy
+							of this software and associated documentation files (the "Software"), to deal
+							in the Software without restriction, including without limitation the rights
+							to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+							copies of the Software, and to permit persons to whom the Software is
+							furnished to do so, subject to the following conditions:
+
+							The above copyright notice and this permission notice shall be included in all
+							copies or substantial portions of the Software.
+
+							THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+							IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+							FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+							AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+							LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+							OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+							SOFTWARE.
+							"""
+						)
+						.frame(maxWidth: .infinity, alignment: .leading)
+						.padding()
+					}
+					.navigationTitle("SwiftUIPullToRefresh")
+				}
+			}
 		}
 	}
 }
