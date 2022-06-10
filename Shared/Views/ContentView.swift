0d2520caed90ae7e699a78a87322b3b880d8b543
Melvin Gundlach <mail@melvin-gundlach.de>
2022-06-10 21:41:24 +0200

    Fix ContentView preview

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -142,7 +142,11 @@ struct ContentView: View {
 }
 
 struct ContentView_Previews: PreviewProvider {
+    private static let api = API()
+    
     static var previews: some View {
         ContentView()
+            .environmentObject(api)
+            .environmentObject(Player(api: api))
     }
 }
