f5edec9c4dc2c126310d8456d56d8801bbe51cc7
Melvin Gundlach <mail@melvin-gundlach.de>
2022-07-12 16:23:50 +0200

    Add ShowChannel App Intent

---
+++ Shared/Intents/ShowChannel.swift
@@ -0,0 +1,34 @@
+//
+//  ShowChannel.swift
+//  NebulaSwift
+//
+//  Created by Melvin Gundlach on 12.07.22.
+//
+
+import Foundation
+import AppIntents
+import SwiftUI
+
+struct ShowChannel: AppIntent {
+	@Parameter(title: "Channel")
+	var channel: Channel
+	
+	static var title: LocalizedStringResource = "Show Channel"
+	
+	static var openAppWhenRun = true
+	
+	@MainActor
+	func perform() async throws -> some IntentResult {
+		let url = URL(string: "NebulaSwift://channel/\(channel.slug)")!
+		#if os(iOS)
+		await UIApplication.shared.open(url)
+		#else
+		await NSApplication.shared.open(url)
+		#endif
+		return .result()
+	}
+	
+	static var parameterSummary: IntentParameterSummary<ShowChannel> {
+		Summary("Show \(\.$channel)")
+	}
+}
