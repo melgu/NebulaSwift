d820fdfd73c29395d36111ecb65076f939e35e50
Melvin Gundlach <mail@melvin-gundlach.de>
2022-12-09 12:35:08 +0100

    Add Watch Later statistics

--- Shared/Views/WatchLater.swift
+++ Shared/Views/WatchLater.swift
@@ -11,6 +11,9 @@ struct WatchLater: View {
 	@EnvironmentObject private var api: API
 	@EnvironmentObject private var player: Player
 	
+	@State private var showStatistics = false
+	@State private var statistics: Statistics?
+	
 	var body: some View {
 		AutoVideoGrid(fetch: { page in
 			try await api.watchLaterVideos(page: page)
@@ -18,6 +21,36 @@ struct WatchLater: View {
 		.assumeWatchLater()
 		.navigationTitle("Watch Later")
 		.onAppear { player.reset() }
+		.toolbar {
+			AsyncButton {
+				guard statistics == nil else {
+					showStatistics = true
+					return
+				}
+				let videos = try await api.watchLaterVideos(page: 1, pageSize: 8_192)
+				statistics = Statistics(
+					count: videos.count,
+					duration: .seconds(videos.map(\.duration).reduce(0, +))
+				)
+				showStatistics = true
+			} label: {
+				Label("Statistics", systemImage: "info.circle")
+			}
+			.asyncButtonStyle(.progress(replacesLabel: true))
+		}
+		.alert("Statistics", isPresented: $showStatistics, presenting: statistics) { _ in
+			Button("OK") {}
+		} message: { statistics in
+			Text("""
+			^[\(statistics.count) videos](inflect: true)
+			Total duration: \(statistics.duration.formatted()) h
+			""")
+		}
+	}
+	
+	private struct Statistics {
+		let count: Int
+		let duration: Duration
 	}
 }
 
