3c9338e2e3c4a1efa660d157adadf8b53d1d777f
Melvin Gundlach <mail@melvin-gundlach.de>
2025-05-16 21:20:08 +0200

    Update user API

--- NebulaSwift/API/API+User.swift
+++ NebulaSwift/API/API+User.swift
@@ -15,18 +15,28 @@ struct UserResponse: Decodable {
 	let accountType: String
 	let promoExpiration: String?
 	let optInToCommunications: Bool
+	let collectVideoAnalytics: Bool
+//	let agreedToTerms: NSNull
 	let trialCancelled: Bool
 	let isSubscribed: Bool
 	let isPasswordSet: Bool
 	let hasCuriositystreamSubscription: Bool
+	let zobjectUserID: String
+//	let zypeConsumer: NSNull
+//	let zypeAuthInfo: NSNull
 	let promotion: String?
 	let iapStatus: String?
+//	let subscription: NSNull
+	let entitlements: [String]
+	let emailVerified: Bool
+	let isStaff: Bool
 }
+
 
 extension API {
 	var user: UserResponse {
 		get async throws {
-			let url = URL(string: "https://api.watchnebula.com/api/v1/auth/user/")!
+			let url = URL(string: "\(storage.authBaseURL)/api/v1/auth/user/")!
 			return try await request(.get, url: url, authorization: .token)
 		}
 	}
