ccdf2486b0bd7c95418821ee100cefb4d9f2f152
Melvin Gundlach <mail@melvin-gundlach.de>
2025-05-16 20:54:29 +0200

    Update app config API

---
+++ NebulaSwift/API/API+AppConfig.swift
@@ -0,0 +1,35 @@
+//
+//  API+AppConfig.swift
+//  NebulaSwift
+//
+//  Created by Melvin Gundlach on 16.05.25.
+//
+
+import Foundation
+
+struct AppConfig: Codable {
+	/// Does **not** contain trailing "/" from my experience
+	let authBaseURL: String
+	
+	/// Does contain trailing "/" from my experience/
+	let contentBaseURL: String
+	
+//	let features: [Any?]
+//	let platform: String
+//	let environment: String
+//	let appStoreURL: String
+//	let signupEnabled: Bool
+//	let minimumVersion: String
+//	let minimumOSVersion: String
+//	let recommendedVersion: String
+//	let minimumOSVersionMessage: String
+}
+
+extension API {
+	var appConfig: AppConfig {
+		get async throws {
+			let url = URL(string: "\(storage.contentBaseURL)app_configs/ios/")!
+			return try await request(.get, url: url, authorization: .none)
+		}
+	}
+}
